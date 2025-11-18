e7693913dba24e0379c2d0ae0f16bd395b08cca0
Michaels042 <mnwachukwu042@gmail.com>
2025-11-18 10:39:41 -0800

    setup the routes and created error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
+import{ createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
+import Results from "./pages/Results";
+import Elections from "./pages/Elections";
+import ElectionDetails from "./pages/ElectionDetails";
+import Candidates from "./pages/Candidates";
+import Congrats from "./pages/Congrats";
+import Logout from "./pages/Logout";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+{
+  path: '/',
+  element: <RootLayout />,
+  errorElement: <ErrorPage />,
+  children: [
+    {
+    index: true,
+    element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
+  },
+  {
+    path: "results",
+    element: <Results />
+  },
+  {
+    path: "elections",
+    element: <Elections />
+  },
+   {
+    path: "elections/:id",
+    element: <ElectionDetails />
+  },
+   {
+    path: "elections/:id/candidates",
+    element: <Candidates />
+  },
+  {
+    path: "congrats",
+    element: <Congrats />
+  },
+  {
+    path: "logout",
+    element: <Logout />
+  },
+  ]
+}
+
+])
+
+
 
 function App() {
-  return (
-   <h1>Hello, World!</h1>
-  );
+  return (<RouterProvider router={router} />);
 }
 
 export default App;
