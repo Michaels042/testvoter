4afeae6e25fec32ba64074b8d33599f0ab91cf7b
Michaels042 <mnwachukwu042@gmail.com>
2025-11-20 10:10:36 -0800

    added css files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className='nav__logo'>VotingAPP</Link>
+            <menu>
+                <NavLink to="/elections">Elections</NavLink>
+                <NavLink to="/results">Results</NavLink>
+                <NavLink to="/logout">Logout</NavLink>
+            </menu>
+            <button className="theme__toggle-btn"></button>
+
+        </div>
+
+    </nav>
   )
 }
 
