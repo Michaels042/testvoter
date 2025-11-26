15307fbf51251b131f8a3c1ad6d05a1610094103
Michaels042 <mnwachukwu042@gmail.com>
2025-11-26 03:35:49 -0800

    navbar, mobile view and dark/ligh mode done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
-import { IoIosMoon, IoIosSunny } from 'react-icons/io'
+import React, { useEffect, useState } from 'react'
+import { IoIosMoon, IoMdSunny } from 'react-icons/io'
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(localStorage.getItem
+    ('voting-app-theme') || "")
+
+    // function to close nav menu on small screens when menu link is clicked
+  const closeNavMenu = () => {
+    if(window.innerWidth < 600){
+      setShowNav(false);
+    } else {
+      setShowNav(true);
+    }
+  }
+
+  // function to change toggle theme
+  const changeThemeHandler = () => {
+    if(localStorage.getItem('voting-app-theme') == 'dark'){
+      localStorage.setItem('voting-app-theme', '')
+    } else {
+      localStorage.setItem('voting-app-theme', 'dark')
+    }
+    setDarkTheme(localStorage.getItem('voting-app-theme', ''))
+  }
+
+  useEffect(() => {
+    document.body.className = localStorage.getItem('voting-app-theme');
+  }, [darkTheme])
+
   return (
     <nav>
         <div className="container nav__container">
             <Link to="/" className='nav__logo'>VotingAPP</Link>
             <div>
-              <menu>
-                  <NavLink to="/elections">Elections</NavLink>
-                  <NavLink to="/results">Results</NavLink>
-                  <NavLink to="/logout">Logout</NavLink>
-              </menu>
-              <button className="theme__toggle-btn"><IoIosMoon /></button>
-              <button className="theme__toggle-btn"><HiOutlineBars3 /></button>
+              {showNav && <menu>
+                  <NavLink to="/elections" onClick={closeNavMenu}>Elections</NavLink>
+                  <NavLink to="/results" onClick={closeNavMenu}>Results</NavLink>
+                  <NavLink to="/logout" onClick={closeNavMenu}>Logout</NavLink>
+              </menu>}
+              <button className="theme__toggle-btn" onClick={changeThemeHandler}>{darkTheme ? <IoMdSunny /> : 
+               <IoIosMoon />}</button>
+              <button className="nav__toggle-btn" onClick={() =>
+                setShowNav(!showNav)}>{showNav ? <AiOutlineClose /> :
+                <HiOutlineBars3 />}</button>
             </div>
         </div>
 
